be38c047d1371146fa364733d7c5c2f94ec7e970
ImAbhishekyadav <abyhlb@gmail.com>
2021-07-07 21:52:01 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CodeForces.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeForces.dir/main.cpp.obj"
+  "CodeForces.exe"
+  "CodeForces.exe.manifest"
+  "CodeForces.pdb"
+  "libCodeForces.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeForces.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
